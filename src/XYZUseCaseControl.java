fa65a540480fd492ea18115fd957160042cf1cbc
Dan Bergh Johnsson <dan.bergh.johnsson@omegapoint.se>
2014-11-24 10:34:38 +0100

    Separata klasser i samma fil Test-dir

--- src/XYZUseCaseControl.java
+++ src/XYZUseCaseControl.java
@@ -6,45 +6,45 @@ import java.sql.*;
  */
 public class XYZUseCaseControl {
 
-    public void doIt() throws AuthenticationService.AuthenticationFailedException {
-        AuthenticationService auth = new AuthenticationService();
+    AuthenticationService auth = new AuthenticationService();
 
+    public void doIt() throws AuthenticationService.AuthenticationFailedException {
+        // ... credentials from user
         String username = "danbj";
         String password = "Vi€e Pre$ident";
+
         auth.signin(username, password);
-
+        // .... some more stuff
     }
+}
 
-    private class AuthenticationService {
-        DataSource dataSource = null;
+class AuthenticationService {
+    DataSource dataSource = null;
 
-        public SecToken signin(String username, String password)
-                throws AuthenticationFailedException {
-            String sql =
-                    String.format("SELECT id FROM iwauser " +
-                            "WHERE username = '%s' AND password='%s'",
-                            username, password);
+    public SecToken signin(String username, String password)
+            throws AuthenticationFailedException {
+        String sql =
+                String.format("SELECT id FROM iwauser " +
+                        "WHERE username = '%s' AND password='%s'",
+                        username, password);
 
-            try {
-                Connection con = dataSource.getConnection();
-                PreparedStatement stmt = con.prepareStatement(sql);
-                ResultSet rs = stmt.executeQuery();
-                if(rs.next()) {
-                    return new SecToken();
-                } else {
-                    throw new AuthenticationFailedException();
-                }
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
+        try {
+            Connection con = dataSource.getConnection();
+            PreparedStatement stmt = con.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            if(rs.next()) {
+                return new SecToken();
+            } else {
+                throw new AuthenticationFailedException();
             }
-        }
-
-        private class SecToken {
-        }
-
-        private class AuthenticationFailedException extends Throwable {
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
+    class SecToken {
+    }
 
+    class AuthenticationFailedException extends Exception {
+    }
 }
