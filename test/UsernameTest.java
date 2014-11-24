c96bdbbd63eaeba838c07488eeae51c65cf323f0
Dan Bergh Johnsson <dan.bergh.johnsson@omegapoint.se>
2014-11-24 12:30:38 +0100

    Enhetstester för valida och invalida Username

---
+++ test/UsernameTest.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UsernameTest {
+/*
+    @Test
+    public void shouldConsiderLowercaseNameAsValid() {
+        new Username("danbjson");
+    }
+*/
+/*
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldConsiderAttackstringInvalid() {
+        new Username("'OR 1=1 --");
+    }
+*/
+}
